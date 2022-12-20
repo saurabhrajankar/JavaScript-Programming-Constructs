2213d9599ed0c1cf2a4bb2f3a3bf735d88b81ecf
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-12-20 13:33:35 +0530

    Places Of Number

---
+++ PlacesOfNumber.js
@@ -0,0 +1,20 @@
+let num=100;
+
+switch(num)
+{
+    case 1:
+        console.log("Units Place");
+        break;
+    case 10:
+        console.log("Tens Place");
+        break;
+    case 100:
+        console.log("Hundreds Place");
+        break;
+    case 1000:
+        console.log("Thousands Place");
+        break;
+    case 100000:
+        console.log("Lakhs Place");
+        break;
+}
